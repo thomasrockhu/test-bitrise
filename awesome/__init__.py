1b0c6d6e36e3a7474ea58d5d2c24f7009add56fb
Tom Hu <tomhu1096@gmail.com>
2020-12-07 23:08:17 -0500

    Add base code and bitrise

---
+++ awesome/__init__.py
@@ -0,0 +1,14 @@
+def smile():
+    return ":)"
+
+def frown():
+    return ":("
+
+def zero():
+    return 0
+
+def one():
+    return 1
+
+def two():
+    return 2
---
+++ tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+import awesome
+
+
+class TestMethods(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(awesome.smile(), ":)")
+
+    def test_frown(self):
+        self.assertEqual(awesome.frown(), ":(")
+
+
+if __name__=='__main__':
+    unittest.main()
